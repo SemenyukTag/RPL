78d1972805d9cbf0c076eb8499c7513eec844c07
airm3sem <Semenyuk_A@mail.ru>
2025-07-27 14:01:29 +0300

    + Проблема с Lombok - решена.

--- src/main/java/org/example/rpl/dto/player/PlayerResponseDTO.java
+++ src/main/java/org/example/rpl/dto/player/PlayerResponseDTO.java
@@ -1,10 +1,18 @@
 package org.example.rpl.dto.player;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.example.rpl.dto.team.SimpleTeamResponseDTO;
 import org.example.rpl.entity.Player.PlayerStatus;
 
 import java.time.LocalDateTime;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlayerResponseDTO {
     private int id;
     private String name;
@@ -15,95 +23,4 @@ public class PlayerResponseDTO {
     private SimpleTeamResponseDTO team;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-
-    // Конструкторы
-    public PlayerResponseDTO() {}
-
-    public PlayerResponseDTO(int id, String name, int rating, String position, int age, 
-                           PlayerStatus status, SimpleTeamResponseDTO team, 
-                           LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
-        this.name = name;
-        this.rating = rating;
-        this.position = position;
-        this.age = age;
-        this.status = status;
-        this.team = team;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    // Геттеры
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public PlayerStatus getStatus() {
-        return status;
-    }
-
-    public SimpleTeamResponseDTO getTeam() {
-        return team;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    // Сеттеры
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setStatus(PlayerStatus status) {
-        this.status = status;
-    }
-
-    public void setTeam(SimpleTeamResponseDTO team) {
-        this.team = team;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
